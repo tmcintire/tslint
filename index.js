fd4539709d88ccbf42ae9074285892296ba5c768
Tim McIntire <tmcintire@aegistg.com>
2020-03-26 15:05:12 -0500

    save

--- index.js
+++
@@ -1,9 +0,0 @@
-var prettier = require("./prettier");
-var tsconfig = require("./tsconfig");
-var tslint = require("./tslint");
-
-module.exports = {
-  prettier,
-  tsconfig,
-  tslint
-};
--- prettier.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  semi: false,
-  arrowParens: "always",
-  bracketSpacing: true,
-  printWidth: 150
-};
