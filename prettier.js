200f6a7119cc9b9dd852a93c9944e8b0da2993c2
Tim McIntire <tmcintire@aegistg.com>
2020-03-26 15:22:12 -0500

    update prettier

---
+++ prettier.js
@@ -0,0 +1,8 @@
+module.exports = {
+  $schema: "http://json.schemastore.org/prettierrc",
+  singleQuote: true,
+  semi: false,
+  arrowParens: "always",
+  bracketSpacing: true,
+  printWidth: 150
+};
